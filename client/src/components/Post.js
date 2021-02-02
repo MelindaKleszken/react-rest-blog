5cc24e9091575ced409faba16b78781597b77b1f
MelindaKleszken <melindakleszken1021@gmail.com>
2021-02-02 21:43:13 +0000

    insomnia tests success

---
+++ client/src/components/Post.js
@@ -0,0 +1,5 @@
+// const {postRouter} = require("..../src/routes/post");  
+// const {listAllPosts, listUserPosts, addNewPost, updatePost, deletePost } = require("..../src/controllers/post");
+
+// const allPost = postRouter.get("/posts", listAllPosts);
+// console.log(allPost);
